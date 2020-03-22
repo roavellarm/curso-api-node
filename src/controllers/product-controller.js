10e991ba3810ad771d729de0edb3c44c7d44282f
Rodrigo Avellar <roavellarm@gmail.com>
2020-03-22 14:25:31 -0300

    Add getAll, getBySlug and getById

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,31 +3,44 @@
 const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
 
+exports.get = (req, res, next) => {
+  Product.find({ active: true }, 'title price slug')
+    .then(data => res.status(200).send(data))
+    .catch(e => res.status(400).send(e))
+}
+
+exports.getBySlug = (req, res, next) => {
+  Product.findOne(
+    { slug: req.params.slug, active: true },
+    'title description price slug tags'
+  )
+    .then(data => res.status(200).send(data))
+    .catch(e => res.status(400).send(e))
+}
+
+exports.getById = (req, res, next) => {
+  Product.findById(req.params.id)
+    .then(data => res.status(200).send(data))
+    .catch(e => res.status(400).send(e))
+}
+
 exports.post = (req, res, next) => {
   let product = new Product(req.body)
   product
     .save()
-    .then(x => {
-      res.status(201).send({
-        message: 'Produto cadastrado com sucesso!'
-      })
-    })
-    .catch(e => {
-      res.status(400).send({
-        message: 'Falha ao cadastrar o produto!',
-        data: e
-      })
-    })
+    .then(x =>
+      res.status(201).send({ message: 'Produto cadastrado com sucesso!' })
+    )
+    .catch(e =>
+      res
+        .status(400)
+        .send({ message: 'Falha ao cadastrar o produto!', data: e })
+    )
 }
 
 exports.put = (req, res, next) => {
   const id = req.param.id
-  res.status(200).send({
-    id: id,
-    item: req.body
-  })
+  res.status(200).send({ id: id, item: req.body })
 }
 
-exports.delete = (req, res, next) => {
-  res.status(200).send(req.body)
-}
+exports.delete = (req, res, next) => res.status(200).send(req.body)
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -5,6 +5,9 @@ const router = express.Router()
 const controller = require('../controllers/product-controller')
 
 // Rotas
+router.get('/', controller.get)
+router.get('/:slug', controller.getBySlug)
+router.get('/admin/:id', controller.getById)
 router.post('/', controller.post)
 router.put('/:id', controller.put)
 router.delete('/', controller.delete)
