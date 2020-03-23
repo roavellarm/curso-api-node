98e64876b212c69a09267061fc3269d49565e8a3
Rodrigo Avellar <roavellarm@gmail.com>
2020-03-23 19:17:43 -0300

    Remove product

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -64,4 +64,12 @@ exports.put = (req, res, next) => {
     })
 }
 
-exports.delete = (req, res, next) => res.status(200).send(req.body)
+exports.delete = (req, res, next) => {
+  Product.findOneAndRemove(req.body.id)
+    .then(x => {
+      res.status(200).send({ message: 'Produto removido com sucesso!' })
+    })
+    .catch(e => {
+      res.status(400).send({ message: 'Falha ao remover produto', data: e })
+    })
+}
