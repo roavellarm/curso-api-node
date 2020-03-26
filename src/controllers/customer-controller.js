f9d5848ed011c120bdd74de554055fbac2797733
Rodrigo Avellar <roavellarm@gmail.com>
2020-03-25 21:50:34 -0300

    Add customer controller

---
+++ src/controllers/customer-controller.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const ValidationContract = require('../validators/fluent-validator')
+const repository = require('../repositories/customer-repository')
+
+exports.post = async (req, res, next) => {
+  let contract = new ValidationContract()
+  contract.hasMinLen(
+    req.body.name,
+    3,
+    'O nome deve conter pelo menos 3 caracteres'
+  )
+
+  contract.isEmail(req.body.email, 'Email inválido')
+
+  contract.hasMinLen(
+    req.body.password,
+    3,
+    'A senha deve conter pelo menos 3 caracteres'
+  )
+
+  if (!contract.isValid()) {
+    res
+      .status(400)
+      .send(contract.errors())
+      .end()
+    return
+  }
+
+  try {
+    await repository.create(req.body)
+    res.status(201).send({ message: 'Cliente cadastrado com sucesso!' })
+  } catch (e) {
+    error(res, e)
+  }
+}
+
+function error(response, error) {
+  response
+    .status(500)
+    .send({ message: 'Falha ao processar sua requisicao', error: error })
+}
