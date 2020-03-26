9b533e940922912d1c66bc73324476c305131d6f
Rodrigo Avellar <roavellarm@gmail.com>
2020-03-26 17:48:03 -0300

    Add md5 to incrypt passwords

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -2,6 +2,7 @@
 
 const ValidationContract = require('../validators/fluent-validator')
 const repository = require('../repositories/customer-repository')
+const md5 = require('md5') // npm install md5 --save
 
 exports.post = async (req, res, next) => {
   let contract = new ValidationContract()
@@ -28,7 +29,11 @@ exports.post = async (req, res, next) => {
   }
 
   try {
-    await repository.create(req.body)
+    await repository.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: md5(req.body.password + global.SALT_KEY)
+    })
     res.status(201).send({ message: 'Cliente cadastrado com sucesso!' })
   } catch (e) {
     error(res, e)
