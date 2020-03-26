34e16f8da65e0fba4694e0c900f825fc26d0e362
Rodrigo Avellar <roavellarm@gmail.com>
2020-03-26 17:29:51 -0300

    Add Orders endpoints

---
+++ src/repositories/order-repository.js
@@ -0,0 +1,14 @@
+'use strict'
+const mongoose = require('mongoose')
+const Order = mongoose.model('Order')
+
+exports.get = async () => {
+  return await Order.find({}, 'number status')
+    .populate('customer', 'name')
+    .populate('items.product', 'title')
+}
+
+exports.create = async data => {
+  let order = new Order(data)
+  await order.save()
+}
